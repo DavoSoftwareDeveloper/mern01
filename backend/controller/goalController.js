f017510d5e0aa77a0ed393444870bef574248786
DavoDev <davidlarrosa58@gmail.com>
2023-02-11 00:32:24 +0100

    rest API for goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,6 +1,10 @@
 const asyncHandler = require('express-async-handler')
 
-const getGoals = asyncHandler( async (req, res) => res.status(200).json({message: 'Get goals'}) )
+const Goal = require('../model/goalModel')
+
+const getGoals = asyncHandler( async (req, res) => {
+    const goals = await Goal.find()
+    res.status(200).json(goals)} )
 
 const setGoal = asyncHandler( async (req, res) => {
     if ( !req.body.text ){
@@ -8,8 +12,10 @@ const setGoal = asyncHandler( async (req, res) => {
      res.status(400)
      throw new Error( 'Please add a text field' )
     }
-    
-    res.status(200).json({message: 'Set goal'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 } )
 
 const updateGoal = asyncHandler( async (req, res) => res.status(200).json({message: `Update goal ${req.params.id}`}) )
